705d88460097f5f17ced679ff5ebcc13202d44d7
Dé Yi Banh <deyi.banh@gmail.com>
2021-11-10 17:10:37 +0100

    build(truffle): Initializing project
    Init the project with Truffle structure and add the Voting contract.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 alyra-defi2-voting-test
+Alyra - London Promo - Defi 2 - Voting Test
---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    }
+  },
+  mocha: {
+    // timeout: 100000
+  },
+  compilers: {
+    solc: {
+      version: "0.8.7",
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        }
+      }
+    }
+  }
+};
